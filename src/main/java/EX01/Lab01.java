feeafb11f692a496f660f7299b88a89958871a72
RupaliLogin123 <rupalipandagre25@gmail.com>
2025-06-13 07:32:35 +0530

    first api script

--- src/main/java/EX01/Lab01.java
+++ src/main/java/EX01/Lab01.java
@@ -1,4 +1,9 @@
 package EX01;
+import io.restassured.RestAssured;
 
-public class La01 {
+public class Lab01 {
+    public static void main(String[] args) {
+        RestAssured.given().baseUri("https://restful-booker.herokuapp.com/ping").when().get()
+                .then().log().all().statusCode(201);
+    }
 }
